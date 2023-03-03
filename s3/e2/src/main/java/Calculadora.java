53eb1b8d3e1b3c6d5e95d7e99fe649ac4f46c5bd
Andrés Ramírez <andres200493@gmail.com>
2023-03-02 21:08:11 -0600

    S3 -> E2

---
+++ s3/e2/src/main/java/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    private CalculadoraDao calculadoraDao;
+    public int suma(int a, int b){
+        return a + b + calculadoraDao.findValorConstante();
+    }
+    public int resta(int a, int b){
+        return a - b + calculadoraDao.findValorConstante();
+    }
+    public int multiplica(int a, int b){
+        return a * b + calculadoraDao.findValorConstante();
+    }
+}
---
+++ s3/e2/src/main/java/CalculadoraDao.java
@@ -0,0 +1,3 @@
+public interface CalculadoraDao {
+    int findValorConstante();
+}
