2a2e82f6821f8db15b220cb7927feba2fc241a39
Andrés Ramírez <andres200493@gmail.com>
2023-03-09 20:08:14 -0600

    S5 -> E1

---
+++ s5/Sesion5/src/main/java/org/bedu/java/backed/Sesion5/config/SaludoConfig.java
@@ -0,0 +1,14 @@
+package org.bedu.java.backed.Sesion5.config;
+
+import org.bedu.java.backed.Sesion5.model.Saludo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SaludoConfig {
+
+    @Bean
+    public Saludo saludo(){
+        return new Saludo("Beto");
+    }
+}
---
+++ s5/Sesion5/src/main/java/org/bedu/java/backed/Sesion5/model/Saludo.java
@@ -0,0 +1,14 @@
+package org.bedu.java.backed.Sesion5.model;
+
+
+public class Saludo {
+    private final String nombre;
+
+    public Saludo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
