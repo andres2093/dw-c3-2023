260a521fff1f3ab01ecc17af425511ceaf2c21d5
Andrés Ramírez <andres200493@gmail.com>
2023-03-16 20:12:14 -0600

    S7 -> R1

--- s7/sesion7/src/main/java/org/bedu/java/backend/sesion7/controller/SaludoController.java
+++ s7/sesion7/src/main/java/org/bedu/java/backend/sesion7/controller/SaludoController.java
@@ -2,6 +2,7 @@ package org.bedu.java.backend.sesion7.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +12,13 @@ public class SaludoController {
     public ModelAndView hola(){
         ModelAndView mav = new ModelAndView("hola");
         mav.addObject("mensaje", "Hola desde thymeleaf!");
+        return mav;
+    }
+
+    @GetMapping("/holaParam")
+    public ModelAndView holaParam(@RequestParam(name = "mensaje") String mensaje){
+        ModelAndView mav = new ModelAndView("hola");
+        mav.addObject("mensaje", mensaje);
         return mav;
     }
 }
