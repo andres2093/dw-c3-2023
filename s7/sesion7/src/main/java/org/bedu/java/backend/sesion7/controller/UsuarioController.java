76ade6576c7be47164fef3e6887969f4e6f20db4
Andrés Ramírez <andres200493@gmail.com>
2023-03-16 21:25:05 -0600

    S7 -> E3

--- s7/sesion7/src/main/java/org/bedu/java/backend/sesion7/controller/UsuarioController.java
+++ s7/sesion7/src/main/java/org/bedu/java/backend/sesion7/controller/UsuarioController.java
@@ -3,9 +3,12 @@ package org.bedu.java.backend.sesion7.controller;
 import org.bedu.java.backend.sesion7.model.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class UsuarioController {
@@ -17,8 +20,12 @@ public class UsuarioController {
     }
 
     @PostMapping("/registro")
-    public ModelAndView registra(Usuario usuario){
-        ModelAndView mav = new ModelAndView("registroExitoso");
+    public ModelAndView registra(@Valid Usuario usuario, Errors errors){
+        String vistaResultado = "registroExitoso";
+        if (errors.hasErrors()){
+            vistaResultado = "index";
+        }
+        ModelAndView mav = new ModelAndView(vistaResultado);
         mav.addObject("usuario", usuario);
         return mav;
     }
